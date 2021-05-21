53dff929af3b425ad4e06220dce0bba72012069a
Dexter Ramos <blitzdex27@gmail.com>
2021-05-21 21:27:51 +0800

    Create SRC directory

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "client", "build")));
+app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
 
 app.get("/", (req, res) => {
   res.sendFile("/index.html");
 });
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server started at port", port));
